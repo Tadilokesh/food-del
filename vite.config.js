8977f0ad2c115dfcd87a9833775ff665395bcf62
Tadi <tadilokesh8@gmail.com>
2025-03-23 20:31:40 +0530

    Added deploy script and updated Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/Food-delivary-Webpage/", // Add your repository name here
 })
